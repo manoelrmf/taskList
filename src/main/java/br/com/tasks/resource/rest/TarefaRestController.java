bb98ee36e2b89dbdb88db2b62c5a93bff5129991
Manoel <manoel.inf1.ep@gmail.com>
2019-11-10 17:40:36 -0300

    Controller com os endpoints de operações de findAll, findById, update, add

---
+++ src/main/java/br/com/tasks/resource/rest/TarefaRestController.java
@@ -0,0 +1,67 @@
+package br.com.tasks.resource.rest;
+
+import br.com.tasks.domain.Tarefa;
+import br.com.tasks.service.TarefaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(
+        value = "/tarefas",
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+        consumes = MediaType.APPLICATION_JSON_UTF8_VALUE
+)
+public class TarefaRestController {
+
+    @Autowired
+    private TarefaService service;
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable("id") Long id) {
+
+        service.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Tarefa editar(@PathVariable("id") Long id, @RequestBody Tarefa tarefa) {
+
+        service.update(id, tarefa);
+        return tarefa;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Tarefa getTarefa(@PathVariable("id") Long id) {
+
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> salvar(@RequestBody Tarefa tarefa) {
+
+        service.save(tarefa);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(tarefa.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Tarefa> listar() {
+        return service.findAll();
+    }
+}
