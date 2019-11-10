c13dfd6a24e4c269900973f86f87543450e3cafe
Manoel <manoel.inf1.ep@gmail.com>
2019-11-10 20:16:36 -0300

    Implementando service de usuário

---
+++ src/main/java/br/com/tasks/service/UsuarioService.java
@@ -0,0 +1,22 @@
+package br.com.tasks.service;
+
+import br.com.tasks.domain.Tarefa;
+import br.com.tasks.domain.Usuario;
+
+import java.util.List;
+
+public interface UsuarioService {
+
+    void save(Usuario usuario);
+
+    void update(Long id, Usuario usuario);
+
+    void delete(Long id);
+
+    Usuario findById(Long id);
+
+    List<Usuario> findAll();
+
+    Usuario updateSenha(Long id, String txSenha);
+
+}
