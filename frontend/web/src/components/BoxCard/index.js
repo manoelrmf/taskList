769fa9126521ccb974a87afbf96c7da5eeb12ba0
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-02-24 02:11:00 -0300

    Update index.js

--- frontend/web/src/components/BoxCard/index.js
+++ frontend/web/src/components/BoxCard/index.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import './style.css'
 
 import Actions from '../Actions'
 
 function BoxCard({ task, handleDelete }){
+    const [titulo, setTitulo] = useState('')
+    const [txDescricao, setTxDescricao] = useState('')
+
+    useEffect(() => {
+        if(task !== undefined){
+            setTitulo(task.titulo)
+            setTxDescricao(task.txDescricao)
+        }
+    }, []);
+  
+       
+
     return(
         <>
             <div className="box-card s-dark">
                 <div className="text-title">
-                    <input name="title" type="text" value={task.titulo} placeholder="Digite seu título" />
+                    <input name="title" type="text" value={titulo}  onChange={e => setTitulo(e.target.value)}  placeholder="Digite seu título" />
                 </div>
                 <div className="text-content">
-                  <textarea name="text" type="text" value={task.txDescricao} placeholder="Descreva sua tarefa ..." rows="4" cols="40" />
+                  <textarea name="text" type="text" value={txDescricao}  onChange={e => setTxDescricao(e.target.value)} placeholder="Descreva sua tarefa ..." rows="4" cols="40" />
                 </div>
                 <Actions id={task.id} handleDelete={handleDelete} />
             </div>
