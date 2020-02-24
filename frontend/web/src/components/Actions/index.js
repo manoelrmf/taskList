ea43a49f01e7cf95fc32fe6d710cc38372dbe39f
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-02-23 23:51:52 -0300

    Atualizando icone de deletar

--- frontend/web/src/components/Actions/index.js
+++ frontend/web/src/components/Actions/index.js
@@ -8,7 +8,7 @@ function Actions(){
            <div className="opcoes">
                 <button className="s-dark">+</button>
                 <button className="s-dark"><img src="./icons/editar.png" className="icons-opcoes" /></button>
-                <button className="s-dark">-</button>
+                <button className="s-dark"><img src="./icons/excluir.png" className="icons-opcoes" /></button>
 
            </div>
         </>
